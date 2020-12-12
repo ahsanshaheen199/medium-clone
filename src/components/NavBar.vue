22778f7a8824fb389c0778c0d107577118b1805d
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2020-12-13 00:27:52 +0600

    navbar active class added for vue router

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,8 +7,9 @@
             </div>
             <div class="w-3/4">
                 <div class="flex justify-end items-center">
-                    <router-link class="px-3 py-2 rounded hover:bg-gray-500" to="/">Home</router-link>
-                    <router-link class="px-3 py-2 rounded hover:bg-gray-500" to="/login">Login</router-link>
+                    <router-link exact-active-class="bg-gray-500" active-class="bg-gray-500" class="px-3 py-2 rounded hover:bg-gray-500" to="/">Home</router-link>
+                    <router-link  exact-active-class="bg-gray-500" active-class="bg-gray-500" class="px-3 py-2 rounded hover:bg-gray-500" to="/login">Login</router-link>
+                    <router-link  exact-active-class="bg-gray-500" active-class="bg-gray-500" class="px-3 py-2 rounded hover:bg-gray-500" to="/register">Register</router-link>
                 </div>
             </div>
         </div>
