53f6e7ea34876abf65e8580ec6ede94271d759a8
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2020-12-12 23:56:25 +0600

    Navbar component added

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="navbar-wrapper bg-black text-white py-4">
+      <div class="container mx-auto">
+        <div class="flex items-center">
+            <div class="w-1/4">
+                <h2>Medium Clone</h2>
+            </div>
+            <div class="w-3/4">
+                <div class="flex justify-end items-center">
+                    <a class="px-3 py-2 rounded hover:bg-gray-500" href="#">Home</a>
+                    <a class="px-3 py-2 rounded hover:bg-gray-500" href="#">New Post</a>
+                    <a class="px-3 py-2 rounded hover:bg-gray-500" href="#">Settings</a>
+                    <a class="px-3 py-2 rounded hover:bg-gray-500" href="#">Sign In</a>
+                    <a class="px-3 py-2 rounded hover:bg-gray-500" href="#">Register</a>
+                </div>
+            </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
