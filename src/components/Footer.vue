5dc13ace37d23247f00a32313eca2a0b789b1c65
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2020-12-21 23:32:45 +0600

    footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="footer bg-black text-white py-4 absolute bottom-0 left-0 w-full">
+        <div class="container mx-auto">
+            <div class="flex justify-center">
+                <p>Copyright 2020 @ahsanshaheen199</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
